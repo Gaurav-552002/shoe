e5ceda1a9cd4dd0a2882a46271a86cf2014c4ae1
Gaurav <gmkulkarni7499@gmail.com>
2026-05-04 17:16:23 +0530

    added the cart secetion

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Main, } from './main/main';
+import { Cart } from './cart/cart';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header, Main],
+  imports: [RouterOutlet,Header, Main, Cart],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,9 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
-export class Cart {}
+export class Cart {
+  total:number=0;
+
+  addedProducts=[
+    {
+      id: 1,
+      image: 'assets/shoe1.jpg' ,
+      name: 'Begin Walk Lite For Men : Black',
+      price:3499,
+      aprice:'₹ 3,499',
+      inStock: true
+    },
+    {
+      id: 2,
+      image: 'assets/shoe2.jpg' ,
+      name: 'Begin Walk Plush For Men : Grey',
+      price:4499,
+      aprice:'₹ 4,499',
+      inStock: false
+    }
+  ]
+
+  subtotal(){
+    this.total = 0;
+    for(let i=0; i<this.addedProducts.length;i++){
+      this.total = this.total + this.addedProducts[i].price;
+    }
+  }
+
+   removeAll(){
+      for(let i=0; i<=this.addedProducts.length; i++){
+        this.addedProducts.pop();
+      }
+    }
+}
